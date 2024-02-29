0eb27026304d9abec68d3a6d0197d88b58f4ebfe
Nur Mohammad <mohammadnur5480@gmail.com>
2024-02-29 22:55:55 +0600

    added service section border

--- src/components/view/home/Service.tsx
+++ src/components/view/home/Service.tsx
@@ -8,8 +8,9 @@ const Service = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 py-8 md:py-16">
       <div className="flex justify-between items-center">
-        <h1 className="text-2xl text-black font-medium sm:text-4xl sm:font-bold ">
+        <h1 className="text-2xl text-black font-medium sm:text-3xl sm:font-bold ">
           Our Services
+        <p className="w-14 h-[3px] md:h-1 md:w-28 lg:w-36 mt-1 bg-main"></p>
         </h1>
         <Link className="no-underline" href="/all-services">
           <Button className="text-primary group shadow-gray-400 shadow-sm text-sm font-semibold gap-1 text-white bg-main flex justify-center border-none items-center">
