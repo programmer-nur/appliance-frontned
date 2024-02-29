96892917afc7713519ef69bd4e6064417edfdcff
Nur Mohammad <mohammadnur5480@gmail.com>
2024-02-29 23:40:03 +0600

    feat: full setup redux in project

--- src/app/lib/Providers.tsx
+++ src/app/lib/Providers.tsx
@@ -1,8 +1,12 @@
 "use client";
+import { store } from "@/redux/store";
 import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <Provider store={store}>
+    <SessionProvider>{children}</SessionProvider>
+  </Provider>;
 };
 
 export default Providers;
