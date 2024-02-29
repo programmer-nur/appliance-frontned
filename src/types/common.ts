eac265ba9644775dc723fe68f1b5d89d5ed6139d
Nur Mohammad <mohammadnur5480@gmail.com>
2024-02-29 23:19:36 +0600

    feat: added common types

---
+++ src/types/common.ts
@@ -0,0 +1,21 @@
+export interface IMeta {
+    limit: number;
+    page: number;
+    total: number;
+  }
+  
+  export interface ResponseSuccessData {
+    data: any;
+    meta?: IMeta;
+  }
+  
+  export interface IGenericErrorResponse {
+    statusCode: number;
+    message: string;
+    errorMessages: IGenericErrorMessage[];
+  }
+  
+  export interface IGenericErrorMessage {
+    path: string | number;
+    message: string;
+  }
